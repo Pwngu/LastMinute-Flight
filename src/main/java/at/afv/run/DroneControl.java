f4d41b588e93044bb15f8f44a57d15a53b2e24d5
Pwngu <klaus.ableitinger@gmail.com>
2015-04-01 15:40:43 +0200

    update project

---
+++ src/main/java/at/afv/run/DroneControl.java
@@ -0,0 +1,12 @@
+package at.afv.run;
+
+/**
+ * Created by klaus on 01.04.15.
+ */
+public class DroneControl {
+
+    public static void main(String[] args) {
+
+        System.out.println("juchuuu");
+    }
+}

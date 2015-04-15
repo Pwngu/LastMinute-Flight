fe274831da062e5b969fb1ee2f4544ffee27047c
FellnerThomas <tfellner@student.tgm.ac.at>
2015-04-15 19:25:50 +0000

    Create Library implementation

--- src/main/java/at/afv/run/DroneControl.java
+++ src/main/java/at/afv/run/DroneControl.java
@@ -4,7 +4,9 @@ package at.afv.run;
  * Created by klaus on 01.04.15.
  */
 public class DroneControl {
-
+    static {
+        System.load("/home/pi/LastMinute-Flight/lib/libcreate.so"); //loads create-library
+    }
     public static void main(String[] args) {
 
         System.out.println("juchuuu");

efb3837127ebd61b6632ea4a601eaab8cd7a0c8a
Pwngu <klaus.ableitinger@gmail.com>
2015-04-17 17:17:24 +0200

    add CreateLib to Classpath

--- src/main/java/at/afv/run/DroneControl.java
+++ src/main/java/at/afv/run/DroneControl.java
@@ -1,14 +1,23 @@
 package at.afv.run;
 
+import static at.afv.createlib.Create.*;
+
 /**
  * Created by klaus on 01.04.15.
  */
 public class DroneControl {
-    static {
-        System.load("/home/pi/LastMinute-Flight/lib/libcreate.so"); //loads create-library
-    }
-    public static void main(String[] args) {
 
+
+    static {
+        System.load("/home/pi/LastMinute-Flight/lib/createlib/libcreate.so"); //loads create-library
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        create_drive_direct(50, 50);
+        Thread.sleep(1000);
+        create_drive_direct(0, 0);
         System.out.println("juchuuu");
     }
 }
